623cd273e5bd7d6b4301215f751ac52c527bdd83
Davidmhz95 <davidmhz95@gmail.com>
2020-07-28 17:50:23 +0200

    Añadidos ngModels para productos

--- code/eMarket.Services/src/app/admin-layout/products/product-form/product-form.component.ts
+++ code/eMarket.Services/src/app/admin-layout/products/product-form/product-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/components/product/product.component';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-product-form',
@@ -6,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.scss']
 })
 export class ProductFormComponent implements OnInit {
+  @Input('typeOfProduct') product: Product;
 
-
+  public id: string
+  public description: string
+  public details: string
+  public stock: number
+  public images: string[]
+  public price: number
+  public offerPrice: number
+  public name : string
   isSaleEnabled: boolean = false
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
+  public addProduct() {
+    //Añadimos los tipos de producto
+    if (!this.dataService.typeOfProduct.map(p => p.name).includes(this.product.name)) {
+      //this.dataService.typeOfProduct.push({ name: this.product.name, properties: this.product.properties })
+    }
+  }
+  
 }
